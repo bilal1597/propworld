eef514807c3a9093d1ba1bc243b8e7a9d9d31e3b
Bilal Ilyas <bilalilyas2597@gmail.com>
2024-11-06 12:43:04 +0500

    admin page with login and authentication

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        if (Auth::check()) {
+            return view('admin.index');
+        } else {
+            return redirect()->route('view.login');
+        }
+    }
+
+    public function getLogin()
+    {
+        if (Auth::check()) {
+            return redirect()->route('view.dashboard');
+        } else {
+            return view('admin.login');
+        }
+    }
+
+    public function postLogin(Request $request)
+    {
+        $user = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:4'
+        ]);
+
+        if (Auth::attempt($user)) {
+
+            return redirect()->route('view.dashboard');
+        } else {
+            return redirect()->route('view.login')
+                ->withErrors([
+                    'email' => 'The provided credentials do not match our system.',
+                ]);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('view.login');
+    }
+
+
+    // public function forget()
+    // {
+    //     return view('admin.forget-password');
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('login', [UserController::class, 'getLogin'])->name('view.login');
+Route::post('login', [UserController::class, 'postLogin'])->name('post.login');
+Route::post('logout', [UserController::class, 'logout'])->name('logout');
+
+// Route::get('forgetpassword', [UserController::class, 'forget'])->name('view.forget');
+
+
+Route::get('dashboard', [UserController::class, 'index'])->name('view.dashboard');
