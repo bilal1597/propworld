50d95cdb3123d861aba73736802bdab3b349772b
Bilal Ilyas <bilalilyas2597@gmail.com>
2024-11-07 10:35:16 +0500

    auth with forgot password

--- resources/views/admin/reset-password.blade.php
+++ resources/views/admin/reset-password.blade.php
@@ -10,22 +10,22 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="dist/css/font_awesome_5_free.min.css">
-    <link rel="stylesheet" href="dist/css/select2.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap-datepicker.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap-timepicker.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap-tagsinput.css">
-    <link rel="stylesheet" href="dist/css/duotone-dark.css">
-    <link rel="stylesheet" href="dist/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="dist/css/iziToast.min.css">
-    <link rel="stylesheet" href="dist/css/fontawesome-iconpicker.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap4-toggle.min.css">
-    <link rel="stylesheet" href="dist/css/summernote-bs4.css">
-    <link rel="stylesheet" href="dist/css/style.css">
-    <link rel="stylesheet" href="dist/css/components.css">
-    <link rel="stylesheet" href="dist/css/spacing.css">
-    <link rel="stylesheet" href="dist/css/custom.css">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/font_awesome_5_free.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap-datepicker.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap-timepicker.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap-tagsinput.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/duotone-dark.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/iziToast.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/fontawesome-iconpicker.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/bootstrap4-toggle.min.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/summernote-bs4.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/components.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/spacing.css')}}">
+    <link rel="stylesheet" href="{{asset('dist/css/custom.css')}}">
 
     <script src="dist/js/jquery-3.6.0.min.js"></script>
     <script src="dist/js/popper.min.js"></script>
